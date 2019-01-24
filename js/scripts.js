2c2e1d2e2a3e1513c3cf32b9a0cb6f99af00e650
Gabriel Santiago <gisantiago@live.com>
2019-01-24 09:49:06 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,25 @@
 /* Treehouse FSJS Techdegree
  * Project 5 - Public API Requests
 ** scripts.js */
+
+/*****
+ * Using JQuery: Added a form element with two input elements to the <div> search-container 
+********/
+$('.search-container').html($('<form>', {action: "#", method: "get"}));
+$('form').html($('<input>', {type: "search", id: "search-input", class: "search-input", placeholder: "Search..."}))
+    .append($('<input>', {type: "submit", value: "🔍", id: "search-submit", class: "search-submit"})); 
+
+/*****
+ * Using JQuery: Added the necessary elements to form the gallery cards - which contain employee's photo, first/last name as well as city/state 
+********/
+$('.gallery').html($('<div>', {class: "card"}));
+$('.card').html($('<div>', {class: "card-img-container"}));
+$('.card-img-container').html($('<img>', {class: "card-img", src: "https://placehold.it/90x90", alt: "profile picture"}));
+$('.card').append($('<div>', {class: "card-info-container"}));
+$('.card-info-container').html($('<h3>first last</h3>', {id: "name", class: "card-name cap"}))
+    .append($('<p>email</p>', {class: "card-text"}))
+    .append($('<p>city, state</p>', {class: "card-text cap"}));
+
+/*****
+ * Using JQuery: Added the necessary elements to form the gallery cards - which contain employee's photo, first/last name as well as city/state 
+********/
